699b7ec81bd0fd6adb7058dd676b127a7ea88a91
mickaelbenaroch <mickaelbenaroch@yahoo.fr>
2019-12-03 21:31:00 +0200

    first approach

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,33 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainPageComponent } from './components/main-page/main-page.component';
+import { Routes, RouterModule } from '@angular/router';
+import { HeaderComponent } from './components/main-page/header/header.component';
+import { FooterComponent } from './components/main-page/footer/footer.component';
+import { HttpServiceService } from './services/http-service/http-service.service';
+import { GaleryComponent } from './components/main-page/galery/galery.component';
+
+const appRoutes: Routes = [
+  { path: '' , component: MainPageComponent},
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainPageComponent,
+    HeaderComponent,
+    FooterComponent,
+    GaleryComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes),
   ],
-  providers: [],
+  providers: [
+    HttpServiceService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
