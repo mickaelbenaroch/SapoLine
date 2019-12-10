28fb0375ba6039c82645fbddf891c1bd36626240
mickaelbenaroch <32894231+mickaelbenaroch@users.noreply.github.com>
2019-12-10 09:44:58 +0200

    changed prod api url
    changed prod api url

--- src/app/services/http-service/http-service.service.ts
+++ src/app/services/http-service/http-service.service.ts
@@ -13,8 +13,8 @@ export class HttpServiceService {
   //#region Lifecycle Hooks
   public constructor(
     protected http: HttpClient,) {
-      //this.apiUrl = "https://sapoappapi.herokuapp.com/";
-      this.apiUrl = "http://localhost:3030/";
+      this.apiUrl = "https://sapoappapi.herokuapp.com/";
+      //this.apiUrl = "http://localhost:3030/";
   }
   //#endregion
 
