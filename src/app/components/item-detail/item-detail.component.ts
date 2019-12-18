41a2ef1b2b914d83ac1d4c69b7da229c86b8ea0f
mickaelbenaroch <mickaelbenaroch@yahoo.fr>
2019-12-18 20:05:11 +0200

    item-details

--- src/app/components/item-detail/item-detail.component.ts
+++ src/app/components/item-detail/item-detail.component.ts
@@ -3,6 +3,7 @@ import { ItemServiceService } from 'src/app/services/item-service/item-service.s
 import { ItemModel } from 'src/app/models/ItemModel';
 import { DomSanitizer } from '@angular/platform-browser';
 import { Router } from '@angular/router';
+import { CategoryEnum } from 'src/app/enums/categories-enum';
 
 @Component({
   selector: 'app-item-detail',
@@ -29,5 +30,31 @@ export class ItemDetailComponent implements OnInit {
       this.itemService.openConfirmation(this.currentItem);
     }
   }
+  
+  itemMenuClicked(ev: CategoryEnum): void {
+    if (ev) {
+      switch(ev) {
+        case CategoryEnum.ShoesAndBags:
+            this.router.navigateByUrl('shoes');
+            break;
+          case CategoryEnum.PantsAndBottoms:
+            this.router.navigateByUrl('bottoms');            
+            break;
+          case CategoryEnum.ShirtsAndTops:
+            this.router.navigateByUrl('tops');    
+            break;
+          case CategoryEnum.CoatsAndJackets:
+            this.router.navigateByUrl('jackets');  
+          break;
+          case CategoryEnum.Accessories:
+            this.router.navigateByUrl('accessories');   
+          break;
+          case CategoryEnum.Dresses:
+            this.router.navigateByUrl('dresses');
+            break;
+      }
+    }
+  }
+
 
 }
